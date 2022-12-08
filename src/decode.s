ab5c9e67bbb725e4698b89b396603ee8b2e35d12
Vulcalien <vulcalien@gmail.com>
2022-12-08 12:56:30 +0100

    Put all the emulator code in IWRAM to increase speed

--- src/decode.s
+++ src/decode.s
@@ -13,7 +13,7 @@
 @ You should have received a copy of the GNU General Public License
 @ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-.text
+.section .iwram, "ax"
 
 @ For each instruction there are 8 bytes:
 @   4 - addressing mode code
